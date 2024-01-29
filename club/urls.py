de9691347038d8379439fec30c1aedcda0d92606
Isai Ortega <ortega.isai@gmail.com>
2024-01-29 01:00:17 -0600

    complemento lo faltante

--- club/urls.py
+++ club/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', MiembrosListView.as_view(),
+    path('', ClubDashboard.as_view(),
          name='miembros'),
     path('miembros', MiembrosListView.as_view(),
          name='miembros-list'),
@@ -13,4 +13,10 @@ urlpatterns = [
     path('new/', MiembroCreateView.as_view(),
          name='miembro-new'),
 
+    path('calendar-cumple', CalendarioCumples.as_view(), name='calendar-cumple'),
+
+    path('familia/New/', FamiliaCreateView.as_view(),
+         name='familia-new'),
+    path('familia/<int:pk>', FamiliaDetailView.as_view(),
+         name='familia-detail'),
 ]
--- club/views.py
+++ club/views.py
@@ -18,6 +18,26 @@ import calendar
 calendar.setfirstweekday(calendar.SUNDAY)
 
 
+class ClubDashboard(PermissionRequiredMixin, TemplateView):
+    permission_required = 'club.view_miembro'
+    template_name = 'club/miembro_dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        familias = models.Familia.objects.all()
+        context['familia_list'] = familias
+        miembros = models.Miembro.objects.all()
+        df_miembros = read_frame(miembros)
+
+        df_miembros_count = df_miembros.groupby(
+            ['tipo_miembro'])['tipo_miembro'].count().reset_index(name='counts').to_dict("index")
+        context['tipo_miembro_count'] = df_miembros_count
+        context['miembro_count'] = models.Miembro.objects.count()
+
+        return context
+
+
 class MiembrosListView(PermissionRequiredMixin, ListView):
     permission_required = 'club.view_miembro'
     model = models.Miembro
@@ -40,3 +60,97 @@ class MiembroCreateView(PermissionRequiredMixin, CreateView):
     model = models.Miembro
     form_class = forms.MiembroAddForm
     template_name = r'generic_form.html'
+
+
+class FamiliaCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'club.add_famailia'
+    model = models.Familia
+    form_class = forms.FamiliaAddForm
+    template_name = r'generic_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('miembros')
+
+
+class FamiliaDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = 'club.view_Familia'
+    model = models.Familia
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        familia = self.object
+        recibo = Recibo.objects.filter(
+            de__in=familia.miembro.all()
+        )
+        # recibo = Recibo.objects.filter(
+        #     recibos__miembro__in=familia.miembro.all()
+        # )
+        conceptopago = ConceptoPago.objects.filter(
+            miembro__in=familia.miembro.all()
+        )
+
+        context['importe_total'] = conceptopago.aggregate(
+            Sum('importe'))
+        context['id_count'] = conceptopago.count()
+        if conceptopago:
+            df = read_frame(conceptopago)
+            pivote_table = pd.pivot_table(
+                df,
+                index=['concepto', ],
+                columns=['miembro', ],
+                values=['importe'],
+                aggfunc=np.sum,
+                fill_value='',
+                margins=True,
+                margins_name='Total',
+            )
+
+            context['reporte_conceptos'] = pivote_table.to_html(
+                classes=["table", "table-sm", "table-striped",
+                         "table-bordered"],
+                border=0,
+                # table_id='dataTable',
+                float_format='${:,.2f}'.format,
+                index=True
+            )
+
+        context['conceptopago_list'] = conceptopago
+        context['recibo_list'] = recibo
+        return context
+
+
+class CalendarioCumples(LoginRequiredMixin, TemplateView):
+    template_name = 'club/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = get_date(self.request.GET.get('month', None))
+        cal = Calendar(d.year, d.month)
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        return context
+
+
+def get_date(req_month):
+    if req_month:
+        year, month = (int(x) for x in req_month.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
