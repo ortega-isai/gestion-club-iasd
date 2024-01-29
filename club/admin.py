126ec471c926b6fa557d8b51e3b06c44243704d4
Isai Ortega <ortega.isai@gmail.com>
2024-01-28 23:58:47 -0600

    Fixes

--- club/admin.py
+++ club/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from .models import Miembro, Familia
+from .models import Miembro, Familia, TipoMiembro, ClaseMiembro
+
+
+@admin.register(TipoMiembro)
+class TipoMiembroAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    model = TipoMiembro
+
+
+@admin.register(ClaseMiembro)
+class ClaseMiembroAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    model = ClaseMiembro
 
 
 @admin.register(Miembro)
 class MiembroAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ['id', 'nombre', 'apellido_paterno', 'apellido_materno',
-                    'fecha_nacimiento', 'sexo', 'tipo_miembro', 'clase', 'miembro_iglesia',
-                    'primera_vez']
+                    'fecha_nacimiento', 'sexo', 'tipo_miembro', 'clase', 'miembro_iglesia']
     list_filter = ["tipo_miembro", "sexo", "clase"]
     search_fields = ['nombre', 'apellido_paterno', 'apellido_materno', 'sexo',
-                     'tipo_miembro', 'clase', 'miembro_iglesia', 'primera_vez']
+                     'tipo_miembro', 'clase', 'miembro_iglesia']
     # fields = ['miembro', 'fecha_pago',
     #           'concepto', 'importe', 'notas']
 
