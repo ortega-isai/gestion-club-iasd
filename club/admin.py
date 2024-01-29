74eec2793561be6aa2d7f6d9d453d575934179a3
Isai Ortega <ortega.isai@gmail.com>
2024-01-28 23:48:25 -0600

    admin for the models

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from .models import Miembro, Familia
+
+
+@admin.register(Miembro)
+class MiembroAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'apellido_paterno', 'apellido_materno',
+                    'fecha_nacimiento', 'sexo', 'tipo_miembro', 'clase', 'miembro_iglesia',
+                    'primera_vez']
+    list_filter = ["tipo_miembro", "sexo", "clase"]
+    search_fields = ['nombre', 'apellido_paterno', 'apellido_materno', 'sexo',
+                     'tipo_miembro', 'clase', 'miembro_iglesia', 'primera_vez']
+    # fields = ['miembro', 'fecha_pago',
+    #           'concepto', 'importe', 'notas']
+
+
+@admin.register(Familia)
+class FamiliaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    autocomplete_fields = ["miembro"]
