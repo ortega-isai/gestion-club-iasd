6dbd3b7e57450bfed1f446619d7f776bda06bec0
Isai Ortega <ortega.isai@gmail.com>
2024-02-01 19:03:15 -0600

    Agrego los reportes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,17 @@ from .views import *
 urlpatterns = [
     path('', DashboardTemplateView.as_view(),
          name='home'),
+    path('reportes', ReportesTemplateView.as_view(),
+         name='reportes'),
+         
+    path('reportes/reporte-pagos-conceptos', PagosConceptoTemplateView.as_view(),
+         name='reporte-pagos-conceptos'),
+    path('reportes/reporte-pagos-cuotas', CuotasTemplateView.as_view(),
+         name='reporte-pagos-cuotas'),
+    path('reportes/reporte-pagos-camporee', CamporeeTemplateView.as_view(),
+         name='reporte-pagos-camporee'),
+    path('reportes/reporte_mensual_pagos', reporte_mensual_pagos,
+         name='reporte_mensual_pagos'),
+    path('reportes/reporte-recoleccion', RecoleccionTemplateView.as_view(),
+         name='reporte-recoleccion'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -77,3 +77,207 @@ class DashboardTemplateView(LoginRequiredMixin, TemplateView):
             suma_ingreso = 0
         context['saldo'] = suma_ingreso - suma_egreso
         return context
+
+
+class ReportesTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/reportes.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class PagosConceptoTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/reportes/reporte_pagoconcepto.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pagos = models.ConceptoEntrada.objects.all()
+        df = read_frame(pagos)
+        pivote_table = pd.pivot_table(
+            df,
+            index=['miembro', ],
+            columns=['concepto', ],
+            values=['importe'],
+            aggfunc=np.sum,
+            fill_value='',
+            margins=True,
+            margins_name='Total',
+        )
+
+        context['resultado'] = pivote_table.to_html(
+            classes=["table", "table-sm", "table-striped",
+                     "table-bordered"],
+            border=0,
+            # table_id='dataTable',
+            float_format='${:,.2f}'.format,
+            index=True
+        )
+
+        return context
+
+
+class CuotasTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/reportes/reporte_cuotas.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pagos = models.ConceptoEntrada.objects.filter(
+            concepto__concepto__contains="Cuota")
+        df = read_frame(pagos)
+        # n_by_state = df.groupby("state")["last_name"].count()
+
+        pivote_table = pd.pivot_table(
+            df,
+            index=['miembro', ],
+            columns=['concepto', ],
+            values=['importe'],
+            aggfunc=np.sum,
+            fill_value='',
+            margins=True,
+            margins_name='Total',
+        )
+
+        context['resultado'] = pivote_table.to_html(
+            classes=["table", "table-sm", "table-striped",
+                     "table-bordered"],
+            border=0,
+            # table_id='dataTable',
+            float_format='${:,.2f}'.format,
+            index=True
+        )
+
+        return context
+
+
+class CamporeeTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/reportes/reporte_cuotas.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pagos = models.ConceptoEntrada.objects.filter(
+            concepto__concepto__contains="Camporee"
+        ).values(
+            'id', 'Recibo', 'miembro', 'miembro__tipo_miembro', 'miembro__clase', 'concepto', 'importe')
+
+        df = read_frame(pagos, fieldnames=[
+                        'id', 'Recibo', 'miembro', 'miembro__tipo_miembro', 'miembro__clase', 'concepto', 'importe'])
+
+        # n_by_state = df.groupby("state")["last_name"].count()
+
+        pivote_table = pd.pivot_table(
+            df,
+            index=['miembro__clase', 'miembro__tipo_miembro', 'miembro', ],
+            columns=['concepto', ],
+            values=['importe'],
+            aggfunc=np.sum,
+            fill_value='',
+            margins=True,
+            margins_name='Total',
+        )
+
+        context['resultado'] = pivote_table.to_html(
+            classes=["table", "table-sm", "table-striped",
+                     "table-bordered"],
+            border=0,
+            # table_id='dataTable',
+            float_format='${:,.2f}'.format,
+            index=True
+        )
+
+        return context
+
+
+@login_required
+def reporte_mensual_pagos(request):
+    context = {}
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = forms.ReporteMensualPagoForm(
+            request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            recibos = models.Entrada.objects.filter(fecha_pago__year=form.cleaned_data['anio'],
+                                                    fecha_pago__month=form.cleaned_data['mes'])
+            conceptopago = models.ConceptoEntrada.objects.filter(
+                Recibo__in=recibos)
+
+            df = read_frame(conceptopago)
+            pivote_table = pd.pivot_table(
+                df,
+                index=['miembro', ],
+                columns=['concepto', ],
+                values=['importe'],
+                aggfunc=np.sum,
+                fill_value='',
+                margins=True,
+                margins_name='Total',
+            )
+
+            context['resultado'] = pivote_table.to_html(
+                classes=["table", "table-sm", "table-striped",
+                         "table-bordered"],
+                border=0,
+                # table_id='dataTable',
+                float_format='${:,.2f}'.format,
+                index=True
+            )
+
+            return render(request, 'dashboard/reportes/reporte_pagomensual.html', context)
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = forms.ReporteMensualPagoForm(
+            initial={
+                "anio": datetime.now().date().year,
+                "mes": datetime.now().date().month
+            }
+        )
+        context['form'] = form
+        return render(request, 'generic_form.html', context)
+
+
+class RecoleccionTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/reportes/reporte_recoleccion.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pagos = models.ConceptoEntrada.objects.filter(
+            concepto__concepto__contains="Recoleccion")
+
+        # print(pagos)
+        df = read_frame(pagos)
+        df2 = df.groupby("miembro")["importe"].sum().reset_index('miembro')
+        # print(df2)
+
+        df2['porcentaje'] = df2['importe'] * 100 / 200
+        df2 = df2.round(2)
+
+        # print(df2.columns)
+        # print(df2.to_dict('records'))
+        context['recoleccion_list'] = df2.to_dict('records')
+        pivote_table = pd.pivot_table(
+            df,
+            index=['miembro', ],
+            columns=['concepto', ],
+            values=['importe'],
+            aggfunc=np.sum,
+            fill_value='',
+            margins=True,
+            margins_name='Total',
+        )
+
+        context['resultado'] = pivote_table.to_html(
+            classes=["table", "table-sm", "table-striped",
+                     "table-bordered"],
+            border=0,
+            # table_id='dataTable',
+            float_format='${:,.2f}'.format,
+            index=True
+        )
+
+        return context
